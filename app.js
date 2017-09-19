85534b18e6f8e7e3ff3397021c08a3ee1513bdcb
Andrew Bloom <blooma116@gmail.com>
2017-09-19 15:58:47 -0700

    Fixed a bug which directed user to re-enter answer regardless of answer.

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 'use strict';
-// Your JS file contains a 'use strict' declaration at the top. Always.
-// Design a guessing game about you that involves FIVE yes/no questions. Be creative and fun and whimsical. Or not. It's up to you! It's your project!
-// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
-// Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
-// Use CSS to style your page as you see fit, to the level of abilities in CSS that you currently have
-// Add, Commit, Push process is being followed; it is evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit.
 
 var userName = prompt('Thanks for checking out my "About Me" page. What\'s your name?');
 console.log('User\'s name is ' + userName);
@@ -13,10 +7,10 @@ alert('Hi, ' + userName + ', nice to meet you. See if you can guess some things 
 
 
 
-//question 1
+// question 1
 var answer1 = prompt('Do I have 2 cats?').toLowerCase();
-console.log('Answer 1 is ' + answer1);
-if ((answer1 !== 'y') || (answer1 !== 'yes') || (answer1 !== 'n') || (answer1 !== 'no')) {
+console.log('The user\'s first answer is ' + answer1 + '. The correct answer is yes.');
+if (answer1 === '') {
   // Ask the user to provide a yes or no answer
   alert('Come on, a simple yes or no will suffice!');
   answer1 = prompt('Do I have 2 cats?').toLowerCase();
@@ -31,10 +25,10 @@ if ((answer1 === 'y') || (answer1 === 'yes')) {
 }
 
 
-
+// question 2
 var answer2 = prompt('Do I have a son?').toLowerCase();
-console.log('Answer 2 is ' + answer2);
-if ((answer2 !== 'y') || (answer2 !== 'yes') || (answer2 !== 'n') || (answer2 !== 'no')) {
+console.log('The user\'s second answer is ' + answer2 + '. The correct answer is no.');
+if (answer2 === '') {
   alert('Come on, a simple yes or no will suffice!');
   answer2 = prompt('Do I have a son?').toLowerCase();
   console.log('User changed answer 2 to ' + answer2);
@@ -46,16 +40,46 @@ if ((answer2 === 'n') || (answer2 === 'no')) {
 }
 
 
-
+// question 3
 var answer3 = prompt('Was I born in Seattle?').toLowerCase();
-console.log('Answer 3 is ' + answer3);
-if ((answer3 !== 'y') || (answer3 !== 'yes') || (answer3 !== 'n') || (answer3 !== 'no')) {
+console.log('The user\'s third answer is ' + answer3 + '. The correct answer is no.');
+if (answer3 === '') {
   alert('Come on, a simple yes or no will suffice!');
   answer3 = prompt('Was I born in Seattle?').toLowerCase();
-  console.log('User changed answer 1 to ' + answer3);
+  console.log('User changed answer 3 to ' + answer3);
 }
 if ((answer3 === 'n') || (answer3 === 'no')) {
   alert('That\'s right! I was born in New Orleans.');
 } else {
   alert('Actually, I was born in New Orleans.');
+}
+
+
+// question 4
+var answer4 = prompt('Am I interested in analog photography?').toLowerCase();
+console.log('The user\'s fourth answer is ' + answer4 + '. The correct answer is yes.');
+if (answer4 === '') {
+  alert('Come on, a simple yes or no will suffice!');
+  answer4 = prompt('Am I interested in analog photography?').toLowerCase();
+  console.log('User changed answer 4 to ' + answer4);
+}
+if ((answer4 === 'y') || (answer4 === 'yes')) {
+  alert('You betcha! I love shooting some film. Also like shooting digital too though! I love all sorts of photography.');
+} else {
+  alert('Actually, I think film is a really cool medium. The chemistry of photography is fascinating.');
+}
+
+
+// question 5
+var answer5 = prompt('Do I have an interest in synthesizers?').toLowerCase();
+console.log('The user\'s fifth answer is ' + answer5 + '. The correct answer is yes.');
+if (answer5 === '') {
+  alert('Come on, a simple yes or no will suffice!');
+  answer5 = prompt('Do I have an interest in synthesizers?').toLowerCase();
+  console.log('User changed answer 5 to ' + answer5);
+}
+if ((answer5 === 'y') || (answer5 === 'yes')) {
+  alert('Totally! Synthesizers are the coolest. I\'d love to build a modular system someday.');
+} else {
+  alert('I really do love them though.');
 }
