64ab0fb4dc4a2109b629382fd25f9f94d7b0aaaa
Andrew Bloom <blooma116@gmail.com>
2017-09-19 16:19:17 -0700

    Removed buggy feature.

--- app.js
+++ app.js
@@ -6,16 +6,9 @@ console.log('User\'s name is ' + userName);
 alert('Hi, ' + userName + ', nice to meet you. See if you can guess some things about me.');
 
 
-
 // question 1
 var answer1 = prompt('Do I have 2 cats?').toLowerCase();
 console.log('The user\'s first answer is ' + answer1 + '. The correct answer is yes.');
-if (answer1 === '') {
-  // Ask the user to provide a yes or no answer
-  alert('Come on, a simple yes or no will suffice!');
-  answer1 = prompt('Do I have 2 cats?').toLowerCase();
-  console.log('User changed answer 1 to ' + answer1);
-}
 if ((answer1 === 'y') || (answer1 === 'yes')) {
   // Tell the user they got the correct answer
   alert('That\'s right!');
@@ -28,11 +21,6 @@ if ((answer1 === 'y') || (answer1 === 'yes')) {
 // question 2
 var answer2 = prompt('Do I have a son?').toLowerCase();
 console.log('The user\'s second answer is ' + answer2 + '. The correct answer is no.');
-if (answer2 === '') {
-  alert('Come on, a simple yes or no will suffice!');
-  answer2 = prompt('Do I have a son?').toLowerCase();
-  console.log('User changed answer 2 to ' + answer2);
-}
 if ((answer2 === 'n') || (answer2 === 'no')) {
   alert('Correct, I do have a daughter though! Her name is Aurora and she is 2.');
 } else {
@@ -43,11 +31,6 @@ if ((answer2 === 'n') || (answer2 === 'no')) {
 // question 3
 var answer3 = prompt('Was I born in Seattle?').toLowerCase();
 console.log('The user\'s third answer is ' + answer3 + '. The correct answer is no.');
-if (answer3 === '') {
-  alert('Come on, a simple yes or no will suffice!');
-  answer3 = prompt('Was I born in Seattle?').toLowerCase();
-  console.log('User changed answer 3 to ' + answer3);
-}
 if ((answer3 === 'n') || (answer3 === 'no')) {
   alert('That\'s right! I was born in New Orleans.');
 } else {
@@ -58,11 +41,6 @@ if ((answer3 === 'n') || (answer3 === 'no')) {
 // question 4
 var answer4 = prompt('Am I interested in analog photography?').toLowerCase();
 console.log('The user\'s fourth answer is ' + answer4 + '. The correct answer is yes.');
-if (answer4 === '') {
-  alert('Come on, a simple yes or no will suffice!');
-  answer4 = prompt('Am I interested in analog photography?').toLowerCase();
-  console.log('User changed answer 4 to ' + answer4);
-}
 if ((answer4 === 'y') || (answer4 === 'yes')) {
   alert('You betcha! I love shooting some film. Also like shooting digital too though! I love all sorts of photography.');
 } else {
@@ -73,13 +51,8 @@ if ((answer4 === 'y') || (answer4 === 'yes')) {
 // question 5
 var answer5 = prompt('Do I have an interest in synthesizers?').toLowerCase();
 console.log('The user\'s fifth answer is ' + answer5 + '. The correct answer is yes.');
-if (answer5 === '') {
-  alert('Come on, a simple yes or no will suffice!');
-  answer5 = prompt('Do I have an interest in synthesizers?').toLowerCase();
-  console.log('User changed answer 5 to ' + answer5);
-}
 if ((answer5 === 'y') || (answer5 === 'yes')) {
   alert('Totally! Synthesizers are the coolest. I\'d love to build a modular system someday.');
 } else {
-  alert('I really do love them though.');
+  alert('But I do!');
 }
