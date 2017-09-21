38b3f70af3f5b8db9433507fa514db6c7f61d519
Seth Donohue <donohue.seth@gmail.com>
2017-09-21 13:56:29 -0700

    Added declaration, and questions 6 & 7 fucntions.

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 'use strict';
+
+//global variables
+var totalRight = 0;
 var userName = '';
 
+//Greeting
 function user() {
   userName = prompt('Thanks for checking out my "About Me" page. What\'s your name?');
   console.log('User\'s name is ' + userName);
   alert('Hi, ' + userName + ', nice to meet you. See if you can guess some things about me.');
 }
 user();
-//console.log(userName);
 
-// var x = 3;
-// var y = 4;
-//
-// function check(i,j) {
-//   return i * j;
-// }
-// //check(x,y);
-// alert(check(x,y));
-var totalRight = 0;
 
 // question 1
 function question1() {
@@ -76,6 +70,7 @@ function question4() {
 }
 question4();
 
+
 // question 5
 function question5() {
   var answer5 = prompt('Do I have an interest in synthesizers?').toLowerCase();
@@ -90,69 +85,78 @@ function question5() {
 question5();
 
 
-//question 6  
-alert('Let\'s play a number guessing game!');
-var attemptNum = 1;
-while (attemptNum < 6) {
-  var answer6 = parseInt(prompt('I\'m going to give you 5 tries to guess how many years I lived in New Orleans before moving to Washington. \nPick a number.'));
-  console.log('The user\'s question 6 guess #' + attemptNum + ' is ' + answer6);
-  if (answer6 === NaN) {
-    alert('Come on, gimme a number, not that garbage! I\'m dinging you a guess for that.');
-    attemptNum++;
-  } else if (answer6 < 13) {
-    alert('I lived there longer than that! You have ' + (5 - attemptNum) + ' guess(es) left.');
-    attemptNum++;
-  } else if (answer6 > 13) {
-    alert('No, I didn\'t live there that long. You have ' + (5 - attemptNum) + ' guess(es) left.');
-    attemptNum++;
+//question 6
+function question6() {
+  alert('Let\'s play a number guessing game!');
+  var attemptNum = 1;
+  while (attemptNum < 6) {
+    var answer6 = parseInt(prompt('I\'m going to give you 5 tries to guess how many years I lived in New Orleans before moving to Washington. \nPick a number.'));
+    console.log('The user\'s question 6 guess #' + attemptNum + ' is ' + answer6);
+    if (answer6 === NaN) {
+      alert('Come on, gimme a number, not that garbage! I\'m dinging you a guess for that.');
+      attemptNum++;
+    } else if (answer6 < 13) {
+      alert('I lived there longer than that! You have ' + (5 - attemptNum) + ' guess(es) left.');
+      attemptNum++;
+    } else if (answer6 > 13) {
+      alert('No, I didn\'t live there that long. You have ' + (5 - attemptNum) + ' guess(es) left.');
+      attemptNum++;
+    } else {
+      alert('That\'s right! I lived in New Orleans for 13 years. You guessed in only ' + attemptNum + ' attempt(s). Good job!');
+      attemptNum += 10;
+    }
+  }
+  if (attemptNum > 10) {
+    alert('You Win!');
+    console.log('The user guessed question 6 correctly in ' + (attemptNum - 10) + ' attempt(s).' );
+    totalRight++;
   } else {
-    alert('That\'s right! I lived in New Orleans for 13 years. You guessed in only ' + attemptNum + ' attempt(s). Good job!');
-    attemptNum += 10;
+    alert('You Lose. :\'( I lived in New Orleans for 13 years.');
+    console.log('The user failed to guess question 6 correctly.');
   }
 }
-if (attemptNum > 10) {
-  alert('You Win!');
-  console.log('The user guessed question 6 correctly in ' + (attemptNum - 10) + ' attempt(s).' );
-  totalRight++;
-} else {
-  alert('You Lose. :\'( I lived in New Orleans for 13 years.');
-  console.log('The user failed to guess question 6 correctly.');
-}
+question6();
 
 
 // question 7
-var ansArr = ['holland', 'the netherlands', 'netherlands', 'belgium', 'france'];
-var guessNum = 1;
-var solved = false;
-alert('Let\'s play another game. I\'m going to give you 6 chances to guess the following question:');
-while (guessNum < 7) {
-  var answer7 = prompt('Can you guess a country I have visited outside of North America?').toLowerCase();
-  console.log('The user\'s question 7 guess #' + guessNum + ' is ' + answer7);
-  for (var i = 0; i < ansArr.length; i++) {
-    var q = ansArr[i];
-    if (q === answer7) {
-      alert('You guessed correctly! And you did it in only ' + guessNum + ' guess(es)!');
-      guessNum += 10;
-      solved = true;
+function question7() {
+  var ansArr = ['holland', 'the netherlands', 'netherlands', 'belgium', 'france'];
+  var guessNum = 1;
+  var solved = false;
+  alert('Let\'s play another game. I\'m going to give you 6 chances to guess the following question:');
+  while (guessNum < 7) {
+    var answer7 = prompt('Can you guess a country I have visited outside of North America?').toLowerCase();
+    console.log('The user\'s question 7 guess #' + guessNum + ' is ' + answer7);
+    for (var i = 0; i < ansArr.length; i++) {
+      var q = ansArr[i];
+      if (q === answer7) {
+        alert('You guessed correctly! And you did it in only ' + guessNum + ' guess(es)!');
+        guessNum += 10;
+        solved = true;
+      }
+    }
+    if (solved === false) {
+      alert('Nice try, but I\'ve never been there before. You have ' + (6 - guessNum) + ' guess(es) left.');
+      guessNum++;
     }
   }
-  if (solved === false) {
-    alert('Nice try, but I\'ve never been there before. You have ' + (6 - guessNum) + ' guess(es) left.');
-    guessNum++;
+  if (guessNum > 10) {
+    alert('You win! I\'ve visited The Netherlands, Belgium and France.');
+    console.log('User guessed question 7 correctly in ' + (guessNum - 10) + ' guess(es).');
+    totalRight++;
+  } else {
+    alert('Bummer, you lost. I\'ve been to The Netherlands, Belgium and France.');
+    console.log('The user failed to guess question 7 correctly.');
   }
 }
-if (guessNum > 10) {
-  alert('You win! I\'ve visited The Netherlands, Belgium and France.');
-  console.log('User guessed question 7 correctly in ' + (guessNum - 10) + ' guess(es).');
-  totalRight++;
-} else {
-  alert('Bummer, you lost. I\'ve been to The Netherlands, Belgium and France.');
-  console.log('The user failed to guess question 7 correctly.');
-}
+question7();
 
 
 // Declaration of total right answers
-alert('Of all 7 questions I asked, you got ' + totalRight + ' questions correct.');
-if (totalRight === 7) {
-  alert('Wow, you got all the answers correct! Do I know you?');
+function declaration(){
+  alert('Of all 7 questions I asked, you got ' + totalRight + ' questions correct.');
+  if (totalRight === 7) {
+    alert('Wow, you got all the answers correct! Do I know you?');
+  }
 }
+declaration();
